9dc8fdd483db048220a4aeae3e231ac3ea30337f
ChristianSchueler <hello@christianschueler.at>
2024-12-29 22:37:21 +0100

    coordinates coming

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ try {
   console.log("Opening i2c bus...");
   const bus = await openPromisified(1);
 
-  let i2cFuncs = await bus.i2cFuncs();
-  console.log("available functions:", i2cFuncs);
+  //let i2cFuncs = await bus.i2cFuncs();
+  //console.log("available functions:", i2cFuncs);
 
   console.log("Scanning for i2c devices...");
   let devices = await bus.scan();
@@ -35,13 +35,36 @@ try {
 
   console.log("ready.");
 
-  let data = Buffer.alloc(32);
+  let data = Buffer.alloc(16);
+  let x = [];
+  let y = [];
+
+  console.log("Reading positions...");
   while (true) {
     await bus.sendByte(camAddress, 0x36);
-    let dataLen = bus.readI2cBlock(camAddress, 0x36, 32, data);
-    console.log(util.inspect(data));
+    let dataLen = await bus.i2cRead(camAddress, 16, data);
+    //console.log(dataLen, util.inspect(data));
 
-    await sleep(100);
+    x[0] = data[1];
+    x[0] += (data[3] & 0x30) << 4;
+    y[0]  =  data[2];
+    y[0] += (data[3] & 0xC0) << 2;
+    x[1]  = data[4];
+    x[1] += (data[6] & 0x30) << 4;
+    y[1]  = data[5];
+    y[1] += (data[6] & 0xC0) << 2;
+    x[2]  = data[7];
+    x[2] += (data[9] & 0x30) << 4;
+    y[2]  = data[8];
+    y[2] += (data[9] & 0xC0) << 2;
+    x[3]  = data[10];
+    x[3] += (data[12] & 0x30) << 4;
+    y[3]  = data[11];
+    y[3] += (data[12] & 0xC0) << 2;
+
+    console.log("positions - 0:", x[0], y[0], "1:", x[1], y[1], "2:", x[2], y[2], "3:", x[3], y[3]);
+
+    await sleep(1000);
   }
 }
 catch (err) {
