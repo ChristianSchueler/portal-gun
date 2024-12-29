cecafd8484b6f32d5b62dfcd5f15c21a5ba2a5d2
ChristianSchueler <hello@christianschueler.at>
2024-12-29 23:18:16 +0100

    added blob size, added data structure

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ export function sleep(duration_ms) {
 	return new Promise(resolve => setTimeout(resolve, duration_ms));
 }
 
+class TrackedPoint {
+  index = 0;        // 0 .. 3
+  valid = false;
+  x = 0;
+  y = 0;
+  size = 0;
+
+  constructor(index) { this.index = index; }
+}
+
+let trackedPoints = new Array(4).fill().map((value, index) => { return new TrackedPoint(index); })
+
 try {
   console.log("Opening i2c bus...");
   const bus = await openPromisified(1);
@@ -27,11 +39,12 @@ try {
 
   console.log("initializing IR cam at adress", camAddress, "...");
   
-  await bus.writeByte(camAddress, 0x30, 0x01);
+  await bus.writeByte(camAddress, 0x30, 0x01);  //
   await bus.writeByte(camAddress, 0x30, 0x08);
   await bus.writeI2cBlock(camAddress, 0x00, sensivityBlock1.length, Buffer.from(sensivityBlock1));
   await bus.writeI2cBlock(camAddress, 0x1A, sensivityBlock2.length, Buffer.from(sensivityBlock2));
-  await bus.writeByte(camAddress, 0x33, 0x33);
+  await bus.writeByte(camAddress, 0x33, 0x03);  // select mode: 1, 3 or 5
+  await bus.writeByte(camAddress, 0x30, 0x08);
 
   console.log("ready.");
 
@@ -43,26 +56,30 @@ try {
   while (true) {
     await bus.sendByte(camAddress, 0x36);
     let dataLen = await bus.i2cRead(camAddress, 16, data);
-    //console.log(dataLen, util.inspect(data));
+    console.log(util.inspect(data));
 
-    x[0] = data[1];
-    x[0] += (data[3] & 0x30) << 4;
-    y[0]  =  data[2];
-    y[0] += (data[3] & 0xC0) << 2;
-    x[1]  = data[4];
-    x[1] += (data[6] & 0x30) << 4;
-    y[1]  = data[5];
-    y[1] += (data[6] & 0xC0) << 2;
-    x[2]  = data[7];
-    x[2] += (data[9] & 0x30) << 4;
-    y[2]  = data[8];
-    y[2] += (data[9] & 0xC0) << 2;
-    x[3]  = data[10];
-    x[3] += (data[12] & 0x30) << 4;
-    y[3]  = data[11];
-    y[3] += (data[12] & 0xC0) << 2;
+    // see https://wiibrew.org/wiki/Wiimote#Data_Formats
+    trackedPoints[0].valid = data[0] != 0xFF;
+    trackedPoints[0].x = data[1] + ((data[3] & 0x30) << 4);
+    trackedPoints[0].y = data[2] + ((data[3] & 0xC0) << 2);
+    trackedPoints[0].size = data[3] & 0x0F;
 
-    console.log("positions - 0:", x[0], y[0], "1:", x[1], y[1], "2:", x[2], y[2], "3:", x[3], y[3]);
+    trackedPoints[1].valid = data[4] != 0xFF;
+    trackedPoints[1].x = data[4] + ((data[6] & 0x30) << 4);
+    trackedPoints[1].y = data[5] + ((data[6] & 0xC0) << 2);
+    trackedPoints[1].size = data[5] & 0x0F;
+
+    trackedPoints[2].valid = data[7] != 0xFF;
+    trackedPoints[2].x = data[7] + ((data[9] & 0x30) << 4);
+    trackedPoints[2].y = data[8] + ((data[9] & 0xC0) << 2);
+    trackedPoints[2].size = data[8] & 0x0F;
+
+    trackedPoints[3].valid = data[10] != 0xFF;
+    trackedPoints[3].x = data[10] + ((data[12] & 0x30) << 4);
+    trackedPoints[3].y = data[11] + ((data[12] & 0xC0) << 2);
+    trackedPoints[3].size = data[11] & 0x0F;
+
+    console.log(trackedPoints);
 
     await sleep(1000);
   }
